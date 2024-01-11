014263ee79ab2d5b694bac8ff004ed70ad2528d8
Daksh-1905 <dakshjain1905@gmail.com>
2024-01-12 00:50:32 +0530

    first commit

--- script.js
+++ script.js
@@ -11,18 +11,21 @@ let gif=document.getElementById("gif")
 let masterSongName=document.getElementById("masterSongName")
 let songItems=Array.from(document.getElementsByClassName("songItem"))
 let songItemPlay=Array.from(document.getElementsByClassName("songItemPlay"))
+let gifPlay=Array.from(document.getElementsByClassName("gifPlay"))
+let songTimeUpdate=document.getElementById("songTimeUpdate")
+let shuffleButton=document.getElementById("shuffle-button")
 
 let songs=[
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumahari Kasam - Salam-e-Ishq", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Tumahari Kasam - Salam-e-Ishq", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Tune Jo Na Kaha - Mohit Chauhan", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Mast Magan - Arijit Singh", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Kal Ho Na Ho - Sonu Nigam", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Tu Aake Dekhle - King", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Maan Meri Jaan - King", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Cheques - Shubh", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Liggi - Ritviz", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Dil Kya Kare (Salam-e-Ishq) - Adnan Sami", filePath: "songs/8.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Closer - The Chainsmokers", filePath: "songs/9.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Love Me Like You Do - Ellie Goulding", filePath: "songs/9.mp3", coverPath: "covers/10.jpg"},
 ]
 
 songItems.forEach((element,i)=>{
@@ -36,12 +39,15 @@ masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused||audioElement.currentTime<=0)
     {
         audioElement.play()
+        masterSongName.innerText=songs[songIndex].songName
         songItemPlay[songIndex].classList.remove("fa-play-circle")
         songItemPlay[songIndex].classList.add("fa-pause-circle")
         masterPlay.classList.remove("fa-play-circle")
         masterPlay.classList.add("fa-pause-circle")
         gif.style.opacity=1
         masterSongName.style.opacity=1
+        gifPlay[songIndex].style.opacity=1
+        songTimeUpdate.style.opacity=1
     }
     else
     {
@@ -52,6 +58,8 @@ masterPlay.addEventListener('click', ()=>{
         masterPlay.classList.add("fa-play-circle")
         gif.style.opacity=0
         masterSongName.style.opacity=0
+        gifPlay[songIndex].style.opacity=0
+        songTimeUpdate.style.opacity=0
     }
 })
 
@@ -60,12 +68,14 @@ masterForward.addEventListener('click',()=>{
     {
         songItemPlay[songIndex].classList.remove("fa-pause-circle")
         songItemPlay[songIndex].classList.add("fa-play-circle")
+        gifPlay[songIndex].style.opacity=0
         songIndex=0
     }
     else
     {
         songItemPlay[songIndex].classList.remove("fa-pause-circle")
         songItemPlay[songIndex].classList.add("fa-play-circle")
+        gifPlay[songIndex].style.opacity=0
         songIndex++;
     }    
     audioElement.src=`songs/${songIndex+1}.mp3`
@@ -78,6 +88,8 @@ masterForward.addEventListener('click',()=>{
     masterPlay.classList.add("fa-pause-circle")
     gif.style.opacity=1
     masterSongName.style.opacity=1
+    gifPlay[songIndex].style.opacity=1
+    songTimeUpdate.style.opacity=1
 })
 
 masterBackward.addEventListener('click',()=>{
@@ -85,12 +97,14 @@ masterBackward.addEventListener('click',()=>{
     {
         songItemPlay[songIndex].classList.remove("fa-pause-circle")
         songItemPlay[songIndex].classList.add("fa-play-circle")
+        gifPlay[songIndex].style.opacity=0
         songIndex=9
     }
     else
     {
         songItemPlay[songIndex].classList.remove("fa-pause-circle")
         songItemPlay[songIndex].classList.add("fa-play-circle")
+        gifPlay[songIndex].style.opacity=0
         songIndex--;
     }    
     audioElement.src=`songs/${songIndex+1}.mp3`
@@ -103,12 +117,89 @@ masterBackward.addEventListener('click',()=>{
     masterPlay.classList.add("fa-pause-circle")
     gif.style.opacity=1
     masterSongName.style.opacity=1
+    gifPlay[songIndex].style.opacity=1
+    songTimeUpdate.style.opacity=1
 })
 
+let temp=0
 audioElement.addEventListener('timeupdate',()=>{
     //Update seekbar
     progress=parseInt((audioElement.currentTime/audioElement.duration)*100)
     myProgressBar.value=progress
+    if(Math.floor(audioElement.duration)/60>=10)
+    {
+        if(Math.floor((audioElement.duration-audioElement.currentTime))%60>=10)
+        {
+            songTimeUpdate.innerText=Math.floor((audioElement.duration-audioElement.currentTime)/60)+":"+Math.floor((audioElement.duration-audioElement.currentTime))%60
+        }
+        else
+        {
+            songTimeUpdate.innerText=Math.floor((audioElement.duration-audioElement.currentTime)/60)+":"+'0'+Math.floor((audioElement.duration-audioElement.currentTime))%60
+        }
+    }
+    else
+    {
+        if(Math.floor((audioElement.duration-audioElement.currentTime))%60>=10)
+        {
+            songTimeUpdate.innerText='0'+Math.floor((audioElement.duration-audioElement.currentTime)/60)+":"+Math.floor((audioElement.duration-audioElement.currentTime))%60
+        }
+        else
+        {
+            songTimeUpdate.innerText='0'+Math.floor((audioElement.duration-audioElement.currentTime)/60)+":"+'0'+Math.floor((audioElement.duration-audioElement.currentTime))%60
+        }
+    }
+    if(temp>=1)
+    {
+        if(myProgressBar.value==myProgressBar.max)
+        {
+            songItemPlay[songIndex].classList.remove("fa-pause-circle")
+            songItemPlay[songIndex].classList.add("fa-play-circle")
+            gifPlay[songIndex].style.opacity=0
+            songIndex=Math.floor(Math.random()*10)
+            audioElement.src=`songs/${songIndex+1}.mp3`
+            audioElement.currentTime=0
+            audioElement.play()
+            masterSongName.innerText=songs[songIndex].songName
+            songItemPlay[songIndex].classList.remove("fa-play-circle")
+            songItemPlay[songIndex].classList.add("fa-pause-circle")
+            masterPlay.classList.remove("fa-play-circle")
+            masterPlay.classList.add("fa-pause-circle")
+            gif.style.opacity=1
+            masterSongName.style.opacity=1
+            gifPlay[songIndex].style.opacity=1
+            songTimeUpdate.style.opacity=1
+        }
+    }
+    else
+    {
+        if(myProgressBar.value==myProgressBar.max)
+        {
+            if(songIndex>=9)
+            {
+                songItemPlay[songIndex].classList.remove("fa-pause-circle")
+                songItemPlay[songIndex].classList.add("fa-play-circle")
+                songIndex=0
+            }
+            else
+            {
+                songItemPlay[songIndex].classList.remove("fa-pause-circle")
+                songItemPlay[songIndex].classList.add("fa-play-circle")
+                songIndex++;
+            }    
+            audioElement.src=`songs/${songIndex+1}.mp3`
+            masterSongName.innerText=songs[songIndex].songName
+            audioElement.currentTime=0
+            audioElement.play()
+            songItemPlay[songIndex].classList.remove("fa-play-circle")
+            songItemPlay[songIndex].classList.add("fa-pause-circle")
+            masterPlay.classList.remove("fa-play-circle")
+            masterPlay.classList.add("fa-pause-circle")
+            gif.style.opacity=1
+            masterSongName.style.opacity=1
+            gifPlay[songIndex].style.opacity=1
+            songTimeUpdate.style.opacity=1
+        }
+    }
 })
 
 myProgressBar.addEventListener('change',()=>{
@@ -119,22 +210,75 @@ const makeAllPlay=()=>{
     Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
         element.classList.remove('fa-pause-circle')
         element.classList.add('fa-play-circle')
+        gifPlay[songIndex].style.opacity=0
     })
 }
 
 Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
     element.addEventListener('click',(e)=>{
-        makeAllPlay()
-        songIndex=parseInt(e.target.id)
-        e.target.classList.remove('fa-play-circle')
-        e.target.classList.add('fa-pause-circle')
-        audioElement.src=`songs/${songIndex+1}.mp3`
-        audioElement.currentTime=0
-        masterSongName.innerText=songs[songIndex].songName
-        audioElement.play()
-        masterPlay.classList.remove("fa-play-circle")
-        masterPlay.classList.add("fa-pause-circle")
-        gif.style.opacity=1
-        masterSongName.style.opacity=1
+        if(audioElement.paused||audioElement.currentTime<=0)
+        {
+            makeAllPlay()
+            songIndex=parseInt(e.target.id)
+            e.target.classList.remove('fa-play-circle')
+            e.target.classList.add('fa-pause-circle')
+            audioElement.src=`songs/${songIndex+1}.mp3`
+            masterSongName.innerText=songs[songIndex].songName
+            audioElement.play()
+            masterPlay.classList.remove("fa-play-circle")
+            masterPlay.classList.add("fa-pause-circle")
+            gif.style.opacity=1
+            masterSongName.style.opacity=1
+            gifPlay[songIndex].style.opacity=1
+            songTimeUpdate.style.opacity=1
+        }
+        else
+        {
+            if(parseInt(e.target.id)==songIndex)
+            {
+                e.target.classList.remove('fa-pause-circle')
+                e.target.classList.add('fa-play-circle')
+                audioElement.pause()
+                gif.style.opacity=0
+                masterSongName.style.opacity=0
+                gifPlay[songIndex].style.opacity=0
+                songTimeUpdate.style.opacity=0
+                masterPlay.classList.add("fa-play-circle")
+                masterPlay.classList.remove("fa-pause-circle")
+            }
+            else
+            {
+                makeAllPlay()
+                songIndex=parseInt(e.target.id)
+                e.target.classList.remove('fa-play-circle')
+                e.target.classList.add('fa-pause-circle')
+                audioElement.src=`songs/${songIndex+1}.mp3`
+                masterSongName.innerText=songs[songIndex].songName
+                audioElement.play()
+                masterPlay.classList.remove("fa-play-circle")
+                masterPlay.classList.add("fa-pause-circle")
+                gif.style.opacity=1
+                masterSongName.style.opacity=1
+                gifPlay[songIndex].style.opacity=1
+                songTimeUpdate.style.opacity=1
+            }
+        }
     })
+})
+
+
+shuffleButton.addEventListener('click',()=>{
+    temp++;
+    songIndex=Math.floor(Math.random()*10)
+    masterSongName.innerText=songs[songIndex].songName
+    audioElement.src=`songs/${songIndex+1}.mp3`
+    audioElement.play()
+    songItemPlay[songIndex].classList.remove("fa-play-circle")
+    songItemPlay[songIndex].classList.add("fa-pause-circle")
+    masterPlay.classList.remove("fa-play-circle")
+    masterPlay.classList.add("fa-pause-circle")
+    gif.style.opacity=1
+    masterSongName.style.opacity=1
+    gifPlay[songIndex].style.opacity=1
+    songTimeUpdate.style.opacity=1
 })
